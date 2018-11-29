815d6736d5130057ab5373636ebf94a32ccf0146
Carly Romig <caro7702@colorado.edu>
2018-11-29 01:31:21 -0700

    had to add files manually

---
+++ clue/cluein/closet/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Clothing(models.Model):
+    type = models.CharField(max_length = 100, blank=False)
+    price = models.IntegerField()
+    weatherChoices = (
+        ('WARM', 'This piece of clothing can be worn in colder temperatures'),
+        ('MEDIUM', 'This piece of clothing can be worn in any temperature'),
+        ('COOL', 'This piece of clothing can be worn in warmer temperatures')
+    )
+    color = models.CharField(max_length=20, blank=True)
+    size = models.CharField(max_length=20, blank=True)
+    weather = models.CharField(max_length=10, choices=weatherChoices, default="SOLD")    #available, sold, restocking
+    issues = models.CharField(max_length=100, default ="No issues") 
+    class Meta:
+        abstract = True
+def __str__(self):
+    return "Type : {0} Price : {1}.format(self.type"
+
+class Shirts(Clothing):
+    pass
+
+class Pants(Clothing):
+    pass
+
+class Shoes(Clothing):
+    pass
